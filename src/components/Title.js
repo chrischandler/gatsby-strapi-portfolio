1e3f626ceb55fa1d2b53708615c71aac15bcf091
Chris Chandler <chris@chrischandler.com>
2020-06-28 22:55:52 +0800

    added menu, hero, and services

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,18 @@
 import React from "react"
 
-const Title = () => {
-  return <h2>title component</h2>
+{
+  /* Receives title as a prop */
+}
+const Title = ({ title }) => {
+  return (
+    <div className="section-title">
+      <h2>
+        {/* Checks for title prop and returns the default if it's not passed */}
+        {title || "default title"}
+      </h2>
+      <div className="under"></div>
+    </div>
+  )
 }
 
 export default Title
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -2,7 +2,8 @@ import React from "react"
 import {
   FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
+  FaStackOverflow,
+  FaGithub,
   FaBehanceSquare,
   FaTwitterSquare,
 } from "react-icons/fa"
@@ -11,25 +12,30 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/chchris",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+    url: "https://www.behance.com",
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+    icon: <FaStackOverflow className="social-icon"></FaStackOverflow>,
     url: "https://www.twitter.com",
   },
   {
     id: 5,
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://www.github.com/chrischandler",
+  },
+  {
+    id: 6,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
     url: "https://www.twitter.com",
   },
